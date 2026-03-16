cf97874f9a2cfdea7cd8d2091a5d2422a3352ef0
pravallikaa4 <m.prav1759@gmail.com>
2026-03-16 15:10:06 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_URL + "/api";
+const BASE_URL = import.meta.env.VITE_API_URL;
   const token = localStorage.getItem("token");
 
   const res = await fetch(`${BASE_URL}${endpoint}`, {
